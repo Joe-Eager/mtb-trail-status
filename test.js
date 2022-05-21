305180ae9466f8e949e12bb18705d49f8d9f98f7
Joe-Eager <joe.eager@bluesparktechnologies.com>
2022-05-21 13:33:56 -0400

    bigg booty

---
+++ test.js
@@ -0,0 +1,13 @@
+const json = {
+    bedford: {
+        status: 'open',
+        tweet: 'kjhsdgfkjhsdklishdfkjhsdfkjhsdfkjhsdkfjhsdkfjhsdkjfh',
+    },
+    royalview: {
+        status: 'closed',
+        tweet: '======================yguasfgiuhasfiguhjasfdijuhafsdiuhjadfsuihjasdfihusadfihjuafsdihujasdfhiujk'
+    }
+}
+
+console.table(json)
+console.log('\x1b[33m%s\x1b[0m', 'hi!')
