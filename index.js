eaf49ba3f59f88c228a5167a933d9642166d7984
Joe-Eager <40171953+Joe-Eager@users.noreply.github.com>
2022-07-13 18:05:50 -0400

    asd

--- index.js
+++ index.js
@@ -1,177 +1,90 @@
-import async from 'async'
-// import axios from "axios";
-import chalk from 'chalk'
-import fs from 'fs'
-import jsonTable from './parks.js'
+import async from 'async';
+import chalk from 'chalk';
+import fs from 'fs';
+import jsonTable from './parks.js';
 
-// const jsonData = JSON.parse(fs.readFileSync("./secret/secret.json", "utf-8"));
-const jsonFile = JSON.parse(fs.readFileSync('./output.json', 'utf-8'))
-const goData = JSON.parse(fs.readFileSync('../scrape/output.json'))
-
-// const url = jsonData.url;
-// const auth = jsonData.auth;
-
-// let dataStuff = {},
-//   dataStuffTwo = {},
-//   dataStuffThree = {},
-//   dataStuffFour = {};
+const jsonFile = JSON.parse(fs.readFileSync('./output.json', 'utf-8'));
+const goData = JSON.parse(fs.readFileSync('../scrape/output.json'));
 
 function msToTime(ms) {
-  let seconds = (ms / 1000).toFixed(1)
-  let minutes = (ms / (1000 * 60)).toFixed(1)
-  let hours = (ms / (1000 * 60 * 60)).toFixed(1)
-  let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1)
-  if (seconds < 60) return seconds + ' sec ago'
-  else if (minutes < 60) return minutes + ' min ago'
-  else if (hours < 24) return hours + ' hrs ago'
-  else return days + ' days ago'
+	let seconds = (ms / 1000).toFixed(1);
+	let minutes = (ms / (1000 * 60)).toFixed(1);
+	let hours = (ms / (1000 * 60 * 60)).toFixed(1);
+	let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
+	if (seconds < 60) return seconds + ' sec ago';
+	else if (minutes < 60) return minutes + ' min ago';
+	else if (hours < 24) return hours + ' hrs ago';
+	else return days + ' days ago';
 }
 
 const isToday = (date) => {
-  const today = new Date()
-  return (
-    date.getDate() === today.getDate() &&
+	const today = new Date();
+	return (
+		date.getDate() === today.getDate() &&
     date.getMonth() === today.getMonth() &&
     date.getFullYear() === today.getFullYear()
-  )
-}
+	);
+};
 
 const scanner = (data, type, bool) => {
-  for (let i = 0; i < 50; i++) {
-    const obj = data[i]
-    const string = obj.tweet
-    const param = jsonTable(string, type)
-    if (param.regex && bool) {
-      const time = obj.time
-      console.log(new Date(time))
-      if (isToday(new Date(obj.time))) {
-        jsonFile[type].time = 'Today'
-      } else {
-        jsonFile[type].time = msToTime(new Date() - new Date(obj.time))
-      }
-      jsonFile[type].tweet = string
-      bool = false
-      if (/open/i.test(string)) {
-        jsonFile[type].status = true
-        console.log(
-          chalk.green(
-            jsonFile[type].name +
+	for (let i = 0; i < 50; i++) {
+		const obj = data[i];
+		const string = obj.tweet;
+		const regex = jsonTable(string, type);
+		if (regex && bool) {
+			const time = obj.time;
+			console.log(new Date(time));
+			if (isToday(new Date(obj.time))) {
+				jsonFile[type].time = 'Today';
+			} else {
+				jsonFile[type].time = msToTime(new Date() - new Date(obj.time));
+			}
+			jsonFile[type].tweet = string;
+			bool = false;
+			if (/open/i.test(string)) {
+				jsonFile[type].status = true;
+				console.log(
+					chalk.green(
+						jsonFile[type].name +
             ' - Open || Last tweeted: ' +
             jsonFile[type].time
-          )
-        )
-      } else {
-        jsonFile[type].status = false
-        console.log(
-          chalk.red(
-            jsonFile[type].name +
+					)
+				);
+			} else {
+				jsonFile[type].status = false;
+				console.log(
+					chalk.red(
+						jsonFile[type].name +
             ' - Closed || Last tweeted: ' +
             jsonFile[type].time
-          )
-        )
-      }
-    }
-  }
-}
+					)
+				);
+			}
+		}
+	}
+};
 
 async.series([
-  //   function (callback) {
-  //     axios({
-  //       method: "GET",
-  //       url: url.cleMetro,
-  //       headers: {
-  //         Authorization: auth,
-  //         "Access-Control-Allow-Origin": true,
-  //       },
-  //     }).then(
-  //       (res) => {
-  //         dataStuff = res.data.data;
-  //         callback(null, dataStuff);
-  //       },
-  //       (err) => {
-  //         console.log(err);
-  //         callback(err, null);
-  //       }
-  //     );
-  //   },
-  //   function (callback) {
-  //     axios({
-  //       method: "GET",
-  //       url: url.eastRim,
-  //       headers: {
-  //         Authorization: auth,
-  //         "Access-Control-Allow-Origin": true,
-  //       },
-  //     }).then(
-  //       (res) => {
-  //         dataStuffTwo = res.data.data;
-  //         callback(null, dataStuffTwo);
-  //       },
-  //       (err) => {
-  //         console.log(err);
-  //         callback(err, null);
-  //       }
-  //     );
-  //   },
-  //   function (callback) {
-  //     axios({
-  //       method: "GET",
-  //       url: url.hampHill,
-  //       headers: {
-  //         Authorization: auth,
-  //         "Access-Control-Allow-Origin": true,
-  //       },
-  //     }).then(
-  //       (res) => {
-  //         dataStuffThree = res.data.data;
-  //         callback(null, dataStuffThree);
-  //       },
-  //       (err) => {
-  //         console.log(err);
-  //         callback(err, null);
-  //       }
-  //     );
-  //   },
-  //   function (callback) {
-  //     axios({
-  //       method: "GET",
-  //       url: url.vulture,
-  //       headers: {
-  //         Authorization: auth,
-  //         "Access-Control-Allow-Origin": true,
-  //       },
-  //     }).then(
-  //       (res) => {
-  //         dataStuffFour = res.data.data;
-  //         callback(null, dataStuffFour);
-  //       },
-  //       (err) => {
-  //         console.log(err);
-  //         callback(err, null);
-  //       }
-  //     );
-  //   },
-  function () {
-    scanner(goData.CMPmtb, 'bedford', true)
-    scanner(goData.CVNPmtb, "eastRim", true);
-    scanner(goData.SMPmountainbike, "hampHill", true);
-    scanner(goData.CMPmtb, "OECR", true);
-    scanner(goData.CMPmtb, "royalView", true);
-    scanner(goData.CMPmtb, "westCreek", true);
-    scanner(goData.VulturesKnobMTB, "vulturesKnob", true);
-    jsonFile.update.time = goData.lastUpdated
-    console.log(goData.lastUpdated)
-    fs.writeFileSync(
-      '../trail-status/src/parts/output.json',
-      JSON.stringify(jsonFile, null, 2),
-      'utf8',
-      function (err) {
-        if (err) {
-          return console.log(err)
-        }
-
-        console.log(new Date().toString())
-      }
-    )
-  }
-])
+	function () {
+		scanner(goData.CMPmtb, 'bedford', true);
+		scanner(goData.CVNPmtb, 'eastRim', true);
+		scanner(goData.SMPmountainbike, 'hampHill', true);
+		scanner(goData.CMPmtb, 'OECR', true);
+		scanner(goData.CMPmtb, 'royalView', true);
+		scanner(goData.CMPmtb, 'westCreek', true);
+		scanner(goData.VulturesKnobMTB, 'vulturesKnob', true);
+		jsonFile.update.time = goData.lastUpdated;
+		console.log(goData.lastUpdated);
+		fs.writeFileSync(
+			'../trail-status/src/parts/output.json',
+			JSON.stringify(jsonFile, null, 2),
+			'utf8',
+			function (err) {
+				if (err) {
+					return console.log(err);
+				}
+				console.log(new Date().toString());
+			}
+		);
+	}
+]);
--- parks.js
+++ parks.js
@@ -1,40 +1,35 @@
 export default function jsonTable(string, type) {
-    const data = {
-        hampHill: {
-            regex: /trail/i.test(string)
-        },
-        eastRim: {
-            regex: /east\srim/i.test(string)
-        },
-        bedford: {
-            regex: /bedford/i.test(string)
-        },
-        royalView: {
-            regex: /royalview/i.test(string)
-        },
-        westCreek: {
-            regex: /west\screek/i.test(string)
-        },
-        OECR: {
-            regex: !/(west\screek)|(royalview)|(bedfor)/i.test(string)
-        },
-        vulturesKnob: {
-            regex: /status/i.test(string)
-        }
-    }
-    if (type === 'hampHill') {
-        return data.hampHill
-    } else if (type === 'eastRim') {
-        return data.eastRim
-    } else if (type === 'bedford') {
-        return data.bedford
-    } else if (type === 'royalView') {
-        return data.royalView
-    } else if (type === 'westCreek') {
-        return data.westCreek
-    } else if (type === 'OECR') {
-        return data.OECR
-    } else if (type === 'vulturesKnob') {
-        return data.vulturesKnob
-    }
+	const regex = {
+		hampHill: /trail/i,
+		eastRim: /east\srim/i,
+		bedford: /bedford/i,
+		royalView: /royalview/i,
+		westCreek: /west\screek/i,
+		OECR: /(erie)|(canal)|(\so.e\s)/ig,
+		vulturesKnob: /status/i,
+	};
+	const data = {
+		hampHill: regex.hampHill.test(string),
+		eastRim: regex.eastRim.test(string),
+		bedford: regex.bedford.test(string),
+		royalView: regex.royalView.test(string),
+		westCreek: regex.westCreek.test(string),
+		OECR: regex.OECR.test(string),
+		vulturesKnob: regex.vulturesKnob.test(string)
+	};
+	if (type === 'hampHill') {
+		return data.hampHill;
+	} else if (type === 'eastRim') {
+		return data.eastRim;
+	} else if (type === 'bedford') {
+		return data.bedford;
+	} else if (type === 'royalView') {
+		return data.royalView;
+	} else if (type === 'westCreek') {
+		return data.westCreek;
+	} else if (type === 'OECR') {
+		return data.OECR;
+	} else if (type === 'vulturesKnob') {
+		return data.vulturesKnob;
+	}
 }
